3602de45ea76e8012207cdc3975d69399e8f4f44
ahmet-tosun <onur.ahmet.25@gmail.com>
2022-12-16 11:46:42 +0300

    Intro to ES2015 Modules

---
+++ brads-projects/08-Microposts/src/app.js
@@ -0,0 +1,13 @@
+// CommonJS Module Syntax
+// const person = require("./myModule1");
+
+// ES2015 Module
+// import { person, sayHello } from "./myModule2";
+// import * as mod from "./myModule2";
+import greeting from "./myModule2";
+
+// console.log(mod.person.name);
+
+// console.log(mod.sayHello());
+
+console.log(greeting);
