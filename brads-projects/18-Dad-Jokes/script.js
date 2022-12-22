41da37177113e921ac2f66904b40f4190127cddd
ahmet-tosun <onur.ahmet.25@gmail.com>
2022-12-22 09:36:13 +0300

    Dad Jokes

---
+++ brads-projects/18-Dad-Jokes/script.js
@@ -0,0 +1,38 @@
+// https://icanhazdadjoke.com
+
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('joke-btn');
+
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
+
+// USING async/await
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+
+  const res = await fetch('https://icanhazdadjoke.com', config);
+
+  const data = await res.json();
+
+  jokeEl.innerHTML = data.joke;
+}
+
+// USING .then()
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   };
+
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke;
+//     });
+// }
